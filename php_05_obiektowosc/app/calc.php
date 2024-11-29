ad58b62e3208f62ce8453608724cc0aeab7eab47
Kieronidas <fedaxdavid@gmail.com>
2024-11-29 20:43:39 +0100

    Delete php_05_obiektowosc directory

--- php_05_obiektowosc/app/calc.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-require_once dirname(__FILE__).'/../config.php'; // Ładowanie konfiguracji
-require_once $conf->root_path.'/app/CalcCtrl.class.php'; // Korzystanie ze ścieżki zdefiniowanej w $conf
-
-$ctrl = new CalcCtrl();
-$ctrl->process();
-?>
